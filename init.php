9775a74b7b2fcef7081f7e2d0d83a092e1d7b55a
monjuaflame <monjuaflame@gmail.com>
2023-10-29 12:13:13 +0600

    setup

---
+++ init.php
@@ -0,0 +1,9 @@
+/*
+Plugin Name: Employe List Slider
+Plugin URI: 
+Author: Ismail Hossain
+Author URI: http://ismailhossaindev.com
+Version: 1.0
+Description: More Easiest to Use Employe List plugin
+*/
+
